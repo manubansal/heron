15dcd1f2871ad6c17d40a4b780074fabbe42c9cb
Manu Bansal <manub686@gmail.com>
2017-04-14 01:23:42 +0000

    merge ILPPacking with trevorpacking

--- heron/packing/src/java/com/twitter/heron/packing/trevorpacking/ILPPacking.java
+++ heron/packing/src/java/com/twitter/heron/packing/trevorpacking/ILPPacking.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.twitter.heron.packing.ilppacking;
+package com.twitter.heron.packing.trevorpacking;
 //package edu.stanford;
 
 import java.io.BufferedReader;
